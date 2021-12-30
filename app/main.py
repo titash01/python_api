7633c854d9c4ce82b81b8bbcd2ac89633cda9348
Titash RC <titash01@live.in>
2021-12-30 18:46:00 +0100

    API updated to work with PostgresDB

--- app/main.py
+++ app/main.py
@@ -1,9 +1,25 @@
 import uuid
+import psycopg2
+import time
 
 from typing import Optional
 from fastapi import FastAPI, Response, status, HTTPException
 from fastapi.params import Body
 from pydantic import BaseModel,types
+from psycopg2.extras import RealDictCursor #gets column name from table
+
+while(True):
+    try:
+        conn = psycopg2.connect(host='localhost',database='fastapi',user='postgres'
+                                ,password='password1231',cursor_factory=RealDictCursor)
+        cursor = conn.cursor()
+        print("database connection was successful!!")
+        break
+    except Exception as error:
+        print("Connection to database failed....")
+        print("Error: ",error)
+        time.sleep(3)
+
 
 
 app = FastAPI() 
@@ -11,7 +27,7 @@ app = FastAPI()
 
 class Post(BaseModel):
     '''validation of incoming post content'''
-    id: types.UUID1 = None
+    id: str
     title: str
     content: str
     published: bool = True # set default to True when optional param missing
@@ -40,16 +56,21 @@ async def root():
 
 @app.get("/posts")
 async def get_posts():
-    return {"data": my_posts}
+    cursor.execute("""SELECT * from products""")
+    posts = cursor.fetchall()
+    return {"data": posts}
 
 @app.post("/posts", status_code=status.HTTP_201_CREATED)
 async def create_post(post: Post):
-    print(post)
-    print(post.dict())
-    post_dict = post.dict()
-    post_dict['id'] = uuid.uuid1()
-    my_posts.append(post_dict)
-    return {"data": post_dict}
+
+    cursor.execute("""INSERT INTO products (title, content, published) VALUES(%s,%s,%s) RETURNING *"""
+    ,(post.title,post.content, post.published))
+
+    new_post = cursor.fetchone()
+
+    conn.commit()
+
+    return {"data": new_post}
     
 @app.get("/posts/latest")
 async def get_latest_post():
@@ -58,33 +79,39 @@ async def get_latest_post():
 
 @app.get("/posts/{id}")
 async def get_posts(id: str, response: Response):
-    post = find_post(id)
-    if not post:
+    cursor.execute("""SELECT * from products WHERE id =%s""",(id),)
+    test_post = cursor.fetchone()
+    #post = find_post(id)
+    if not test_post:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id:{id} was not found.")
-    return {"post_detail": post}
+    return {"post_detail": test_post}
 
 @app.delete("/posts/{id}", status_code=status.HTTP_204_NO_CONTENT)
 async def delete_post(id: str):
     # deleting post#
-    index = find_index_post(id)
+    cursor.execute("""DELETE FROM products WHERE id = %s returning *""", id,)
 
-    if index == None:
+    deleted_post = cursor.fetchone()
+    
+    conn.commit()
+
+    if deleted_post == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id:{id} does not exist")
 
-    my_posts.pop(index)
     return Response(status_code=status.HTTP_204_NO_CONTENT)
 
-@app.put("/posts/{id}")
+@app.put("/posts/{id}", status_code=status.HTTP_200_OK)
 async def get_post(id: str,post: Post):
-    index = find_index_post(id)
+
+    cursor.execute("""UPDATE products set title =%s,content=%s,published=%s WHERE id =%s RETURNING *""",
+                    (post.title,post.content,post.published,id),)
+    index = cursor.fetchone()
+
     if index == None:
         raise HTTPException(status_code=status.HTTP_404_NOT_FOUND, 
                             detail=f"post with id:{id} does not exist")
     
-    post_dict = post.dict()
-    post_dict['id'] = id
-    my_posts[index] = post_dict
-
-    return {"data": post_dict}
+    conn.commit()
+    return {"data": index, "status": "resource updated successfully"}
