31f75204ab295c424c787c333bb941b541767291
Titash RC <titash01@live.in>
2022-04-15 13:39:24 +0200

    ingestion & mock_api pipeline

---
+++ ingest.py
@@ -0,0 +1,23 @@
+import requests
+import psycopg2
+
+with requests.get("http://127.0.0.1:5000/very_large_request/1000000", stream=True) as r:
+
+    conn = psycopg2.connect(host="localhost",port="5433",dbname="stream_test", user="postgres",password="postgres")
+    cur = conn.cursor()
+    sql = "INSERT INTO transactions(txid, uid, amount) VALUES (%s, %s, %s)"
+
+    cur.execute("truncate table public.transactions;")
+    conn.commit()
+
+    buffer = ""
+    for chunk in r.iter_content(chunk_size=1):
+        if chunk.endswith(b'\n'):
+            t = eval(buffer)
+            print(t)
+            cur.execute(sql, (t[0],t[1],t[2]))
+            conn.commit()
+            buffer = ""
+        else:
+            buffer += chunk.decode()
+
---
+++ mock_api.py
@@ -0,0 +1,23 @@
+from flask import Flask, Response, stream_with_context
+import time
+import uuid
+import random
+
+APP = Flask(__name__)
+
+@APP.route("/very_large_request/<int:rowcount>", methods=["GET"])
+def get_large_request(rowcount):
+    """returns N rows of data"""
+    def f():
+        """generates mock data"""
+        for _ in range(rowcount):
+            time.sleep(0.1)
+            txid = uuid.uuid4()
+            print(txid)
+            uid = uuid.uuid4()
+            amount = round(random.uniform(-1000,1000), 2)
+            yield f"('{txid}','{uid}','{amount}')\n"
+    return Response(stream_with_context(f()))
+
+if __name__ == "__main__":
+    APP.run(debug=True)
